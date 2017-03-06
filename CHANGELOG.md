7993e3fb219f421708641597a4ab334c3690c8d4
Kyle Marshall <me@kylemarshall.me.uk>
2017-03-06 22:10:40 +0000

    Plugin Scaffold

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Cloner Changelog
+
+## 0.0.1 -- 2017.03.06
+
+* Initial release
+
+Brought to you by [Kyle Marshall](kylemarshall.me.uk)
---
+++ translations/en.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Cloner plugin for Craft CMS
+ *
+ * Cloner Translation
+ *
+ * @author    Kyle Marshall
+ * @copyright Copyright (c) 2017 Kyle Marshall
+ * @link      kylemarshall.me.uk
+ * @package   Cloner
+ * @since     0.0.1
+ */
+
+return array(
+    'Translate me' => 'To this',
+);
